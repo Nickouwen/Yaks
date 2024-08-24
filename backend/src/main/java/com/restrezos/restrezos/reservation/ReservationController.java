2e82299e258bbb23937d4670f0b413e69f599924
Nicolaas <71842732+Nickouwen@users.noreply.github.com>
2024-08-24 14:44:51 -0700

    Created basic reservation CRUD handling

--- backend/src/main/java/com/restrezos/restrezos/reservation/ReservationController.java
+++ backend/src/main/java/com/restrezos/restrezos/reservation/ReservationController.java
@@ -1,12 +1,23 @@
 package com.restrezos.restrezos.reservation;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @RestController
+@RequestMapping("/api/reservations")
 public class ReservationController {
 
     private final ReservationRepository reservationRepository;
@@ -14,9 +25,41 @@ public class ReservationController {
     public ReservationController(ReservationRepository reservationRepository) {
         this.reservationRepository = reservationRepository;
     }
-
-    @GetMapping("/api/reservations")
+    
+    @GetMapping("")
     List<Reservation>  findAll() {
         return reservationRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    Reservation findById(@PathVariable Integer id) {
+        Optional<Reservation> reservation = reservationRepository.findById(id);
+        if(reservation.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return reservation.get();
+    }
+
+    // post
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@RequestBody Reservation reservation) {
+        reservationRepository.create(reservation);
+    }
+
+    // put
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@RequestBody Reservation reservation, @PathVariable Integer id) {
+        reservationRepository.update(reservation, id);
+    }
+
+    // delete
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id) {
+        reservationRepository.delete(id);
+    }
+
+
 }
--- backend/src/main/java/com/restrezos/restrezos/reservation/ReservationRepository.java
+++ backend/src/main/java/com/restrezos/restrezos/reservation/ReservationRepository.java
@@ -3,6 +3,7 @@ package com.restrezos.restrezos.reservation;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
@@ -15,6 +16,25 @@ public class ReservationRepository {
 
     List<Reservation> findAll() {
         return reservations;
+    }
+
+    Optional<Reservation> findById(Integer id) {
+        return reservations.stream().filter(r -> r.id().equals(id)).findFirst();
+    }
+
+    void create(Reservation reservation) {
+        reservations.add(reservation);
+    }
+
+    void update(Reservation reservation, Integer id) {
+        Optional<Reservation> existingReservation = findById(id);
+        if(existingReservation.isPresent()) {
+            reservations.set(reservations.indexOf(existingReservation.get()), reservation);
+        }
+    }
+
+    void delete(Integer id) {
+        reservations.removeIf(reservation -> reservation.id().equals(id));
     }
 
     @PostConstruct
